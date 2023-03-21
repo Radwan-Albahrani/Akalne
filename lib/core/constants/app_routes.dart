957ab8826d31d00eaddfe4113eccbddda7336815
Pali2002 <ali77dhamen@hotmail.com>
2023-03-21 22:12:22 +0300

    feat: add AppRoutes class to keep tracking of all screens

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const String loginScreen = "/";
+  static const String signUpScreen = "/signup";
+
+  static const String homeScreenRecipient = "/home-recipient";
+  static const String homeScreenDonor = "/home-donor";
+}
