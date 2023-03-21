239f33c80a44b7669865fc0e03a0d2f55d167d3b
Pali2002 <ali77dhamen@hotmail.com>
2023-03-21 22:11:24 +0300

    feat: sign up screen done

--- lib/core/features/auth/screens/sign_up_screen.dart
+++ lib/core/features/auth/screens/sign_up_screen.dart
@@ -1,4 +1,8 @@
+import 'package:akalne/core/constants/app_routes.dart';
+import 'package:akalne/core/features/auth/screens/widgets/rounded_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -8,14 +12,117 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+
+
+
+  void navgaiteToLoginScreen(BuildContext context) {
+    Navigator.of(context).pop();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
-      ),
-      body: const Center(
-        child: Text('Sign Up Screen'),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: IntrinsicHeight(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Lottie.asset(
+                "assets/lottieIcons/LoginIcon.json",
+                height: 200.h,
+                width: 200.w,
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "Sign In to your Account",
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+              ),
+              Form(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RoundedFormField(
+                        controller: nameController,
+                        hintText: "Your name",
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      RoundedFormField(
+                        controller: phoneController,
+                        hintText: "phone number",
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      RoundedFormField(
+                        controller: emailController,
+                        hintText: "Email",
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      RoundedFormField(
+                        controller: passwordController,
+                        hintText: "Password",
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      SizedBox(
+                        height: 40.h,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text("Login",
+                              style: Theme.of(context).textTheme.labelLarge),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Already have an account?",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          TextButton(
+                            onPressed: () => navgaiteToLoginScreen(context),
+                            child: Text(
+                              "Sign In",
+                              style: Theme.of(context).textTheme.labelMedium,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
