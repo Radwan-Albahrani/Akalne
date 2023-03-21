ba957246178d4beda121e2a4219fd90843b7fdb1
Pali2002 <ali77dhamen@hotmail.com>
2023-03-21 16:03:03 +0300

    storage provider for uploading images

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,3 @@
+class AppConstants {
+  static const lottieLogin = "assets/lottieIcons/LoginIcon.json";
+}
---
+++ lib/core/constants/firebase_constants.dart
@@ -0,0 +1,5 @@
+class FirebaseConstants {
+  static const usersCollection = 'users';
+  static const resturantsCollection = 'resturants';
+  static const ordersCollection = 'orders';
+}
