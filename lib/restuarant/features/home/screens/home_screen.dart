a0d6018354710513d34ccd9c846ffeed84658732
Pali2002 <ali77dhamen@hotmail.com>
2023-03-27 02:17:47 +0300

    home screen and fixes

--- lib/restuarant/features/home/screens/home_screen.dart
+++ lib/restuarant/features/home/screens/home_screen.dart
@@ -1,8 +1,14 @@
+import 'package:akalne/recipient/features/Profile/screens/profile_screen.dart';
+import 'package:akalne/restuarant/features/food/screens/published_meals_screen.dart';
+import 'package:akalne/restuarant/features/profile/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 import '../../../../core/features/auth/controller/auth_controller.dart';
+import '../../../../theme/app_colors.dart';
+import '../../menu/screens/restuarant_menu_screen.dart';
 
 class RestaurantHomeScreen extends ConsumerStatefulWidget {
   const RestaurantHomeScreen({super.key});
@@ -11,6 +17,16 @@ class RestaurantHomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<RestaurantHomeScreen> {
+  var _selectedIndex = 0;
+
+  var screens = [
+    const PublishedMealsScreen(),
+    const RestuarantMenuScreen(),
+    const Center(
+      child: Text('Cart'),
+    ),
+    const RestuarantProfileScreen(),
+  ];
   void logout() {
     ref.read(authControllerProvider.notifier).logout();
   }
@@ -18,38 +34,66 @@ class _HomeScreenState extends ConsumerState<RestaurantHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: GNav(
-        gap: 8,
-        activeColor: Colors.white,
-        iconSize: 24,
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-        duration: const Duration(milliseconds: 800),
-        tabBackgroundColor: Colors.grey[800]!,
-        tabs: const [
-          GButton(
-            icon: Icons.home,
-            text: 'Home',
+      body: screens[_selectedIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+          ],
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
           ),
-          GButton(
-            icon: Icons.search,
-            text: 'Search',
+        ),
+        height: 70.h,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GNav(
+            gap: 16,
+            activeColor: AppColors.light['primary'],
+            color: Colors.grey[400]!,
+            iconSize: 24,
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+            duration: const Duration(milliseconds: 800),
+            tabBackgroundColor: AppColors.light['primaryTransparent'],
+            tabs: const [
+              GButton(
+                icon: Icons.home_outlined,
+                iconSize: 35,
+              ),
+              GButton(
+                icon: Icons.favorite_border_outlined,
+                iconSize: 35,
+              ),
+              GButton(
+                icon: Icons.shopping_cart_outlined,
+                iconSize: 35,
+              ),
+              GButton(
+                icon: Icons.person_outline,
+                iconSize: 35,
+              ),
+            ],
+            selectedIndex: 0,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
           ),
-          GButton(
-            icon: Icons.shopping_cart,
-            text: 'Cart',
-          ),
-          GButton(
-            icon: Icons.person,
-            text: 'Profile',
-          ),
-        ],
-        selectedIndex: 0,
-        onTabChange: (index) {
-          // setState(() {
-          //   _selectedIndex = index;
-          // });
-        },
+        ),
       ),
+      floatingActionButton: _selectedIndex == 0
+          ? FloatingActionButton(
+              backgroundColor: AppColors.light['primary'],
+              onPressed: () {},
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            )
+          : null,
     );
   }
 }
