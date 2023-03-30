6ba8a182c81223bb2daf14e4025c14ab7525b93a
Radwan-Albahrani <Radwan158@gmail.com>
2023-03-30 21:04:13 +0300

    Comments and error checking

--- lib/recipient/features/homeMenu/controller/home_menu_controller.dart
+++ lib/recipient/features/homeMenu/controller/home_menu_controller.dart
@@ -17,20 +17,26 @@ final homeMenuControllerProvider =
           homeMenuRepository: ref.watch(homeMenuRepositoryProvider),
         ));
 
+// Provider to get menu items as a stream
 final menuItemsProvider = StreamProvider((ref) {
   final controller = ref.watch(homeMenuControllerProvider.notifier);
   return controller.getMenuItems();
 });
 
+// Provider to get menu items by restaurant ID
 final ordersProvider = StreamProvider((ref) {
   final controller = ref.watch(homeMenuControllerProvider.notifier);
   final user = ref.watch(userProvider);
   return controller.getOrders(user!.id);
 });
 
+// Controller class for UI related to home Menu
 class HomeMenuController extends StateNotifier<bool> {
+  // Variables
   final Ref _ref;
   final HomeMenuRepository _homeMenuRepository;
+
+  // Constructor
   HomeMenuController({
     required Ref ref,
     required HomeMenuRepository homeMenuRepository,
@@ -38,23 +44,33 @@ class HomeMenuController extends StateNotifier<bool> {
         _homeMenuRepository = homeMenuRepository,
         super(false);
 
+  // Method to get all menu items as a stream from repository
   Stream<List<PublishedMealModel>> getMenuItems() {
     return _homeMenuRepository.getMenuItems();
   }
 
+  // Method to get all orders as a stream from repository
   Stream<List<OrderModel>> getOrders(String id) {
     return _homeMenuRepository.getOrders(id);
   }
 
+  // Method to get all menu items by restaurant ID as a future from repository
   Future<List<PublishedMealModel>> getMenuItemsByID(String id) async {
     return _homeMenuRepository.getMenuItemsByID(id);
   }
 
+  // Method to reserve a meal
   void reserveMeal(
       MenuItemModel meal, int quantity, BuildContext context) async {
+    // Set state to true
     state = true;
-    final userID = _ref.read(userProvider)!.id;
+
+    // Get the user ID and get the orders of that user
+    UserModel? user = _ref.read(userProvider);
+    final userID = user!.id;
     final orderResults = await _homeMenuRepository.getLatestOrder(userID);
+
+    // Use that to get the latest order from that user
     OrderModel? latestOrder;
     orderResults.fold((l) {
       if (l.message != "No orders found") {
@@ -62,24 +78,42 @@ class HomeMenuController extends StateNotifier<bool> {
       }
     }, (r) => latestOrder = r);
 
+    // If the order exists, check if its less than 24 hrs old
     if (latestOrder != null) {
       if (DateTime.now()
               .difference(DateTime.parse(latestOrder?.dateCreated as String))
               .inHours <
           24) {
+        // If it is, don't allow user to order
         Future.delayed(Duration.zero, () {
           showSnackBar(
             context,
             "You can only reserve a meal once every 24 hours",
           );
         });
+        state = false;
         return;
       }
     }
-    UserModel? user = _ref.read(userProvider);
+
+    // If the order doesn't exist or is more than 24 hrs old, allow user to order
+    // Get order count for the order number
     final resultID = await _homeMenuRepository.getOrdersCount();
+
+    // Set the ID to the latest order count
     int id = 0;
-    resultID.fold((l) => showSnackBar(context, l.message), (r) => id = r);
+    bool failed = false;
+    resultID.fold((l) {
+      showSnackBar(context, l.message);
+      failed = true;
+    }, (r) => id = r);
+
+    // If an error happens while getting the ID, show error and exit function
+    if (failed) {
+      return;
+    }
+
+    // Prepare order model
     OrderModel order = OrderModel(
       id: id,
       status: "Sent to Restaurant",
@@ -89,7 +123,10 @@ class HomeMenuController extends StateNotifier<bool> {
       restaurantID: meal.restaurant.id as String,
       user: user as UserModel,
     );
+
+    // Reserve the meal
     var result = await _homeMenuRepository.reserveMeal(order);
+    state = false;
     result.fold(
       (l) => showSnackBar(context, l.message),
       (r) {
